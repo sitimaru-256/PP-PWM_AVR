849d753cde1c554421e93084c4c50236641fb73b
Sitimaru_256 <verderayo0703@outlook.jp>
2026-08-18 09:10:06 +0900

    Add files via upload

---
+++ Src/lut.h
@@ -0,0 +1,19 @@
+#ifndef LUT_H
+#define LUT_H
+
+extern const int _7alpha[127][7];
+extern const int _6alpha[127][6];
+extern const int _5alpha[509][5];
+extern const int _4alpha[509][4];
+extern const int _3alpha[509][3];
+extern const int _2alpha[509][2];
+extern const int _1alpha[509][1];
+extern const int _7alpha_pole[127];
+extern const int _6alpha_pole[127];
+extern const int _5alpha_pole[509];
+extern const int _4alpha_pole[509];
+extern const int _3alpha_pole[509];
+extern const int _2alpha_pole[509];
+extern const int _1alpha_pole[509];
+
+#endif
